b63511b1df95cf6c887559aa0d11705699490845
umutbasal <umut.bsl@yandex.com>
2021-02-11 20:42:30 +0300

    added styled components support

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import classNames from 'classnames'
 import {Button as FButton} from '@heetch/flamingo-react'
-import styles from './Button.module.scss'
+import styled from 'styled-components'
+
+export const FlamingoButton = styled(FButton)`
+  width: 100%;
+  color: red;
+`;
 
 export const Button = ({ children, className, ...args }) => (
-  <FButton intent={'primary'} className={classNames(styles.ButtonRed, className)} args={args}>{children}</FButton>
+  <FlamingoButton intent={'primary'} className={classNames(className)} args={args}>{children}</FlamingoButton>
 );
