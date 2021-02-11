f71530ac2fea82cb731ad490f96a73228a7a1744
umutbasal <umut.bsl@yandex.com>
2021-02-11 21:31:20 +0300

    static pages

---
+++ pages/blog.jsx
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+
+import { getSortedPostsData } from '../lib/posts'
+import Layout, { siteTitle } from '../components/Layout/Layout'
+import Button from '../components/Button/Button'
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+export default function Blog({ allPostsData }) {
+    return (
+        <Layout home>
+
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <Button>Hello</Button>
+            <section>
+                <h2>Blog</h2>
+                <ul>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
+}
