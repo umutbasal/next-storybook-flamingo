67088f07a517fbc1b60ddd72730e2593dbd7a6cb
umutbasal <umut.bsl@yandex.com>
2021-02-11 21:30:47 +0300

    remove unnecessary

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -22,7 +22,6 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
